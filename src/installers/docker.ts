7ec77ac6511f909803823955f69cc91375153d5d
ymgn-dev <ymgn.dev@gmail.com>
2024-07-14 14:13:33 +0900

    chore: update docker installation

--- src/installers/docker.ts
+++ src/installers/docker.ts
@@ -7,19 +7,19 @@ export const installDocker = async () => {
       return execa(
         `
         # Add Docker's official GPG key:
-        sudo -S apt update
-        sudo -S apt install ca-certificates curl gnupg
-        sudo -S install -m 0755 -d /etc/apt/keyrings
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo -S gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-        sudo -S chmod a+r /etc/apt/keyrings/docker.gpg
+        sudo apt-get update
+        sudo apt-get install ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
 
         # Add the repository to Apt sources:
         echo \
-        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-        sudo -S tee /etc/apt/sources.list.d/docker.list > /dev/null
-        sudo -S apt-get update
-        sudo -S apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+          sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
         `,
         { shell: true },
       );
